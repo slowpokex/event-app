92788000d10f871b7e940b832d2df8751939031c
Mikita Isakau <harley26.gold@gmail.com>
2018-11-19 20:33:10 +0300

    Add timestamp

--- src/app/models/event.ts
+++ src/app/models/event.ts
@@ -1,7 +1,9 @@
+import { Timestamp } from './timestamp';
+
 export interface Event {
   id: number;
   name: string;
   description: string;
   picture: string;
-  creationDate: Date;
+  creationDate: Timestamp;
 }
